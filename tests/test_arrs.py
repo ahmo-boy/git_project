76744f8cc64d6bfb035835b404d74d34e3623a82
ahmed@bebop <ahmedkachkow@gmail.com>
2024-04-14 15:25:30 +0300

    залил файлы

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,47 @@
-from utils import arrs
+import pytest
+from your_module import get, my_slice
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+def test_get_existing_index():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 2) == 3
 
+def test_get_non_existing_index():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 10) is None
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_get_with_default():
+    array = [1, 2, 3, 4, 5]
+    assert get(array, 10, "default") == "default"
+
+def test_my_slice_without_arguments():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll) == [1, 2, 3, 4, 5]
+
+def test_my_slice_with_start():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, 2) == [3, 4, 5]
+
+def test_my_slice_with_negative_start():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, -3) == [3, 4, 5]
+
+def test_my_slice_with_end():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, end=3) == [1, 2, 3]
+
+def test_my_slice_with_negative_end():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, end=-2) == [1, 2, 3]
+
+def test_my_slice_with_start_and_end():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, 1, 4) == [2, 3, 4]
+
+def test_my_slice_with_negative_start_and_end():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, -4, -1) == [2, 3, 4]
+
+def test_my_slice_with_invalid_arguments():
+    coll = [1, 2, 3, 4, 5]
+    assert my_slice(coll, 10, 20) == []
